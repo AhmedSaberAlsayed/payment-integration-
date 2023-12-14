782ce0cd9c2b36c3f07e36f219c9d29694bcc5f0
Ahmed Saber Alsayed <aahmedo284@gmail.com>
2023-12-14 20:04:04 +0200

    done

---
+++ app/Http/Controllers/StripeController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\payment;
+use \Stripe\Checkout\Session;
+use \Stripe\Stripe;
+use Illuminate\Http\Request;
+
+class StripeController extends Controller
+{
+    public function checkout()
+    {
+        return view('checkout');
+    }
+
+    public function session(Request $request)
+    {
+        Stripe::setApiKey(config('stripe.sk'));
+
+        $productname = $request->get('productname');
+        $totalprice = $request->get('total');
+        $two0 = "00";
+        $total = "$totalprice$two0";
+
+        $session = Session::create([
+            'line_items'  => [
+                [
+                    'price_data' => [
+                        'currency'     => 'USD',
+                        'product_data' => [
+                            "name" => $productname,
+                        ],
+                        'unit_amount'  => $total,
+                    ],
+                    'quantity'   => 1,
+                ],
+            ],
+            'mode'        => 'payment',
+            'success_url' => route('success'),
+            'cancel_url'  => route('checkout'),
+        ]);
+        return redirect()->away($session->url);
+    }
+    public function success()
+    {
+        return "Thanks for you order You have just completed your payment. The seeler will reach out to you as soon as possible";
+    }
+}
